2a7322b3e8ee3adcf5b4c3d10b7a4f20511023c8
Greger <gr3ger@gmail.com>
2026-04-17 13:35:42 +0200

    Update twitch_scroll_fix.user.js
    Add back old class name as well, just in case.

--- twitch_scroll_fix.user.js
+++ twitch_scroll_fix.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gr3ger/
 // @match       *://*.twitch.tv/*
 // @grant       none
-// @version     1.0.1
+// @version     1.0.2
 // @author      Greger
 // @homepageURL  https://github.com/gr3ger/userscripts
 // @updateURL    https://raw.githubusercontent.com/gr3ger/userscripts/main/twitch_scroll_fix.user.js
@@ -17,7 +17,8 @@ const originalAddEventListener = EventTarget.prototype.addEventListener;
 EventTarget.prototype.addEventListener = function(type, listener, options) {
     if (type == 'scroll' && this?.className?.includes('scrollable-area') == true)
     {
-        if (this?.firstChild?.className?.includes('chat-scrollable-area__message-container') == true)
+        if (this?.firstChild?.className?.includes('chat-scrollable-area__message-container') == true
+            || this?.firstChild?.className?.includes('area__message-container') == true)
         {
             var stack = new Error().stack;
             if (stack?.includes('assets.twitch.tv') == true)
